8079f6f2860eaba941042f6286422f5efd8c13cc
sawantanushil <113376359+sawantanushil@users.noreply.github.com>
2022-10-05 22:33:39 +0530

    First Program execution in JS
    First Program execution in JS

---
+++ Asingnment1.js
@@ -0,0 +1,6 @@
+console.log("My name is Anushil Baburao Sawant.");
+console.log("My hobbies are Bike Ridding,Playing Cricket and Traveling.");
+console.info("My strength is self confidence,smart work and positive attitude.");
+console.warn("My weakness is, i can easily belive every one,Emotional.");
+console.log(30+50);
+console.log(49*479);
