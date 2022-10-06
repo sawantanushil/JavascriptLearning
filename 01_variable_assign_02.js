3c044a4a19ae91e43370685e3a26a0301f100df1
sawantanushil <113376359+sawantanushil@users.noreply.github.com>
2022-10-06 14:29:09 +0530

    First Day of Session
    First Day of Session

---
+++ 01_variable_assign_02.js
@@ -0,0 +1,17 @@
+var my_friend="Sweety";
+var your_friend="Cutie";
+var temp=my_friend;
+console.log(" Before swap the string is",my_friend,your_friend );
+my_friend=your_friend;
+your_friend=temp; 
+console.log("After swap the string is", my_friend,your_friend);
+var num1=100;
+var num2=200;
+var num3=300;
+var temp=num1;
+
+console.log("Before swapping value",num1,num2,num3);
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("After swapping value",num1,num2,num3);
