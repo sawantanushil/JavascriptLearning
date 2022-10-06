363fa7e8df04b7c2ec03956572b3c8b2c6b15c2a
sawantanushil <113376359+sawantanushil@users.noreply.github.com>
2022-10-06 09:27:26 +0530

    swapping variables demo
    swapping variables demo

---
+++ 01_swap_veriable.js
@@ -0,0 +1,11 @@
+var a=100;
+var b=200;
+console.log("Befor swap");
+console.log("Value of A & B is",a,b);
+console.log("After swap");
+var c=a;
+a=b;
+b=c;
+c=a;
+console.log("Value of A & B is ",a,b);
+
---
+++ 01_veriable.js
@@ -0,0 +1,6 @@
+var my_apple;                   //veriable Declaration.
+my_apple="Apple";               //veriable initilization.
+var name="Anushil Sawant";      // veriable Declaration and Initilization on same Line.
+my_name="Anushil Baburao Sawant";
+console.log(name);
+console.log(my_apple);
