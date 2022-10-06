e75629f1a0dcdb79fde49f0bfddf63b0a693d715
sawantanushil <113376359+sawantanushil@users.noreply.github.com>
2022-10-06 09:44:06 +0530

    01_A1_decleration,initilization and Update variables
    01_A1_decleration,initilization and Update variables

---
+++ 01_A1_swapping_veriable.js
@@ -0,0 +1,9 @@
+var myNumber;
+myNumber=100;
+console.log(myNumber);
+var myLove="I love only JS";
+console.log(myLove);
+var temp=200;
+console.log("Before Updating Value of Temp",temp);
+temp="JavaScript";
+console.log("After Updating Value of Temp",temp);
